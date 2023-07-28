0007e98f3aeafb35184235edc05f8c45040cc6ab
annikapalm <annika.palm@hotmail.com>
2023-07-28 08:23:31 +0200

    First commit
    First commit - setting up the project, basic folder structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+          <h2>Lucy </h2>
+          <h3>~ Your smart dream journal ~</h3>
+          <p>Journal your dreams and explore your dreamscape. Lucy's smart analytics take care of the rest.</p>
+      </div>
+  );
+}
+
+export default App;
