014c508f72beb9a595722173ac5a5f8141e6e947
sosafe-annika-palm <annika.palm@hotmail.com>
2023-08-01 13:41:57 +0200

    Basic shell
    Build basic app shell for future design

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import './App.css';
 
 function App() {
   return (
-      <div className="App">
+      <div>
+        <header>
           <h2>Lucy </h2>
           <h3>~ Your smart dream journal ~</h3>
           <p>Journal your dreams and explore your dreamscape. Lucy's smart analytics take care of the rest.</p>
+        </header>
+        <nav>
+          <ol>
+            <li>Menu item 1</li>
+            <li>Menu item 2</li>
+            <li>Menu item 3</li>
+          </ol>
+        </nav>
+        <form>
+          <label>Label 1</label>
+          <input type="text"></input>
+          <label>Label 2</label>
+          <input type="text"></input>
+          <label>Label 3</label>
+          <input type="text"></input>
+        </form>
+        <footer></footer>
       </div>
   );
 }
