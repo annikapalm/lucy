4d5a9a862ce31a04a0bd610c11da2609a0c003cc
sosafe-annika-palm <annika.palm@hotmail.com>
2023-08-17 16:21:46 +0200

    Display of dummy data
    Mapping function added to display list of dummy data. CSS styling added and improved.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
-import file from "./dummy_data.json";
-console.log(file[1].text)
+import dummy_data from './dummy_data.js';
+//console.log(file[1].text)
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
           <h3>~ Your smart dream journal ~</h3>
           <p>Journal your dreams and explore your dreamscape. Lucy's smart analytics take care of the rest.</p>
         </header>
-        <nav class="nav">
+        <nav className="nav">
           <ul>
             <li>Menu item 1</li>
             <li>Menu item 2</li>
@@ -29,7 +29,11 @@ function App() {
           <input type="text"></input>
           <button type="submit">Submit</button>
         </form>
-        <div class="dream_container"></div>
+        <div className="dream_container">
+          {dummy_data.map((dream, index) => (
+            <div className="dream" key={`${dream.title}-${index}`}><p>{dream.title}</p></div>
+          ))}
+        </div>
         <footer></footer>
       </div>
   );
