acad568a7085edc0837bcb35f248d82d535f6dc7
sosafe-annika-palm <annika.palm@hotmail.com>
2023-08-24 12:07:24 +0200

    Styling of text boxes
    Added styling to text boxes and form. Improved CSS. Added dreams to dummy data file.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
   return (
       <div className='App'>
         <header>
-          <h2>Lucy </h2>
-          <h3>~ Your smart dream journal ~</h3>
-          <p>Journal your dreams and explore your dreamscape. Lucy's smart analytics take care of the rest.</p>
+          <h1>Lucy </h1>
+          <h2>~ Your smart dream journal ~</h2>
+          <h3>Journal your dreams and explore your dreamscape. Lucy's smart analytics take care of the rest.</h3>
         </header>
         <nav className="nav">
           <ul>
@@ -21,17 +21,24 @@ function App() {
           </ul>
         </nav>
         <form>
-          <label>Label 1</label>
+          <label>Title</label>
           <input type="text"></input>
-          <label>Label 2</label>
-          <input type="text"></input>
-          <label>Label 3</label>
+          <label>Date</label>
+          <input type="date"></input>
+          <label>Text</label>
+          <input type="text" placeholder="Describe your dream..."></input>
+          <label>Rating</label>
           <input type="text"></input>
           <button type="submit">Submit</button>
         </form>
         <div className="dream_container">
           {dummy_data.map((dream, index) => (
-            <div className="dream" key={`${dream.title}-${index}`}><p>{dream.title}</p></div>
+            <div className="dream_entry" key={`${dream.title}-${index}`}>
+              <p className="dream_title">{dream.title}</p>
+              <p className="dream_date">{dream.date}</p>
+              <p className="dream_text">{dream.text}</p>
+              <p className="dream_rating">{dream.rating}</p>
+              </div>
           ))}
         </div>
         <footer></footer>
--- src/dummy_data.js
+++ src/dummy_data.js
@@ -8,7 +8,7 @@ const dummy_data =
         "date" : "08/14/2023",
         "rating" : "haunting",
         "image" : "none"
-    },
+},
 
 {
         "title" : "dreaming about fruit flies",
@@ -16,7 +16,7 @@ const dummy_data =
         "date" : "08/12/2023",
         "rating": "confusing",
         "image": "none"
-    },
+},
 
 {
         "title" : "Chasing a star in space as a last wish before death",
@@ -24,9 +24,24 @@ const dummy_data =
         "date" : "08/08/2023",
         "rating": "beautiful",
         "image": "none"
-    }
+},
 
-
+{
+    "title" : "dreamed that I was a different person",
+    "text" : "I had a dream once and I was some random guy like in my 30s or 40s and I had 2 young kids and a wife and I don’t remember a lot of it but I remember what my house looked like and stuff and I remember coming home and the kids hugging me but not much else and I’ve heard that dreams are made up of things that you remember or that you’ve seen before but everything I saw in that dream like the house I just have no memory of ever seeing I’m thinking it could be like a past life dream or something if you believe in that idk but it’s just so weird to me and when I woke up I was so disconnected from reality and it took me a while to get used to it again and when I woke up I was like where even am I like where are my kids and stuff like that",
+    "date" : "08/04/2023",
+    "rating": "confusing",
+    "image": "none"
+},
+/*
+{
+    "title" : "Really weird, unsettling dream about disappearing old man",
+    "text" : "Dimly lit room, something like a basement. In the middle of the room is a table. On the table is something like a wooden sarcophagus or a coffin. There’s no lid, and in it lies an old man. His eyes are closed as if resting. Entirely surrounding the table are men, they are soldiers. They aren’t dressed in military uniforms, and I know not what they’re soldiers for, but they’re soldiers. Each of the soldiers, all of them, have both of their hands on some part of the old man’s body, as though holding him down. They’re holding him all down his arms and legs, and on his torso, it’s a bit crowded. It feels very necessary that they do this, but it is unclear why. The old man is completely unmoving, not resisting or anything at all. After a moment there’s a voice. It is a horrible, deep, booming voice. It’s the voice of the old man, but it is not spoken. The old man’s face is motionless, his eyes still closed, but the voice is heard. It is not spoken, but it is in the minds of all of the soldiers. They all hear this terrible voice. I don’t remember exactly what the voice says, but it is something about a fire. A great, burning fire. The voice ceases, and after a moment the old man’s body suddenly becomes invisible. It’s as though he had disappeared, yet the hands of the soldiers don’t move. They seem to be still holding and leaning on old man’s body, but it’s become completely unseen and invisible. A brief moment later, that invisible body is suddenly gone completely. The soldiers’ hands are holding nothing at all, and their hands abruptly reach the bottom of the sarcophagus, as when something you’re putting pressure on is pulled out from under you. There is a moment of confusion, and then the sound of footsteps. The soldiers can hear the sound of bare feet on the cold, concrete floor. They look to the sound of the footsteps and see no one, nothing at all. There’s another footstep, and another. The footsteps are approaching one of the soldiers. The footsteps stop just before the soldier. The soldier is seemly unaffected, and there are no more footsteps. Gone, whatever it was. Dream ends there. I awoke with tears in my eyes, but not feeling scared, super weird.",
+    "date" : "07/15/2023",
+    "rating": "confusing, weird",
+    "image": "none"
+}
+*/
     ]
 
     export default dummy_data;
